b0beeec70b8d33a7bc740153c83163178b8598f1
Kyle Cook <Kyle.Cook@iam.local>
2018-09-26 15:19:54 -0500

    Movement changes

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour {
 
-	// Use this for initialization
+    [SerializeField]
+    private int lives = 3;
+
+    [SerializeField]
+    private string name = "Player 1";
+
+    [SerializeField]
+    private float jumpHeight = 5, speed = 3;
+
+    private bool hasKey;
+
+    private bool isOnGround;
+
+    private Rigidbody2D rigidbody2DInstance;
+
+    private float horizontalInput;
+
+    // Use this for initialization
 	void Start () {
-        Debug.Log("Hello");
-	}
+        rigidbody2DInstance = GetComponent<Rigidbody2D>();
+
+        rigidbody2DInstance.gravityScale = 5;
+   	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        //transform.Translate(0, -0.01f, 0);
+        GetInput();
+        Move();
 	}
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void Move()
+    {
+        rigidbody2DInstance.velocity = new Vector2(horizontalInput, 0);
+    }
 }
