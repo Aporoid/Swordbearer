942824b2789a636bf82c3ef78026d225698b2997
Kyle Cook <Kyle.Cook@iam.local>
2018-10-03 13:44:49 -0500

    Rewritten code

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -2,47 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCharacter : MonoBehaviour {
+public class PlayerCharacter : MonoBehaviour
+{
 
     [SerializeField]
-    private int lives = 3;
+    private float speed = 5;
 
     [SerializeField]
-    private string name = "Player 1";
-
-    [SerializeField]
-    private float jumpHeight = 5, speed = 3;
-
-    private bool hasKey;
-
-    private bool isOnGround;
-
-    private Rigidbody2D rigidbody2DInstance;
+    private Rigidbody2D rb2d;
 
     private float horizontalInput;
 
     // Use this for initialization
-	void Start () {
-        rigidbody2DInstance = GetComponent<Rigidbody2D>();
+	void Start ()
+    {
 
-        rigidbody2DInstance.gravityScale = 5;
    	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        //transform.Translate(0, -0.01f, 0);
-        GetInput();
-        Move();
+        horizontalInput = Input.GetAxis("Horizontal");
+
 	}
 
-    private void GetInput()
+    private void FixedUpdate()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        rb2d.AddForce(Vector2.right * horizontalInput * speed);
     }
 
-    private void Move()
-    {
-        rigidbody2DInstance.velocity = new Vector2(horizontalInput, 0);
-    }
 }
