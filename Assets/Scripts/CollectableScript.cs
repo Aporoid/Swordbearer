e60b23a0df2e2efc594585cdce74794914a7027f
Kyle Cook <Kyle.Cook@iam.local>
2018-11-07 13:50:39 -0600

    Collectable added

---
+++ Assets/Scripts/CollectableScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableScript : MonoBehaviour {
+
+    private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            audioSource.Play();
+            Destroy(spriteRenderer);
+        }
+    }
+}
