0b92fd36884e95a1c7007888c365deabce264b32
Kyle Cook <Kyle.Cook@iam.local>
2018-11-07 13:01:45 -0600

    Book animation checkpoint
    Made a book do a thing.

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
 
+    Animator anim;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if (collision.CompareTag("Player"))
         {
             Debug.Log("Entered checkpoint");
             PlayerCharacter playerCharacter = collision.gameObject.GetComponent<PlayerCharacter>();
             playerCharacter.SetCurrentCheckpoint(this);
+            anim.SetBool("IsPicked", true);
         }
+    }
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
     }
 }
